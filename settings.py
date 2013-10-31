4da55a959bb958add77d85c06140e3027c679c6b
nkadakia <Nisha.Kadakia@intel.com>
2013-10-31 00:39:30 -0700

    updated settings.py file with values

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
-user=''
-password=''
-host=''
-port=
-ssl_options=
-ssl=
+user='nova'
+password='nova'
+host='integ1ocicc01.cps.intel.com'
+port=5671
+ssl_options={'ca_certs': '/opt/openstack/ssl/ca-certificates.crt', 'cert_reqs': 2}
+ssl=True
 
 

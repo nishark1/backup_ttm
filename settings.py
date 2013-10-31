29aece5dd19d42db569b4233c24594b2e9e7ba72
root <root@hznnode01.novalocal>
2013-10-31 15:59:21 -0700

    updated ssl certca path

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ user='nova'
 password='nova'
 host='integ1ocicc01.cps.intel.com'
 port=5671
-ssl_options={'ca_certs': '/opt/openstack/ssl/ca-certificates.crt', 'cert_reqs': 2}
+ssl_options={'ca_certs': '/opt/OpenCloudDashboard/ssl/cacert.pem', 'cert_reqs': 2}
 ssl=True
 
 

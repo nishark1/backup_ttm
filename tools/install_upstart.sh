3e8cdb88a5fa1ae904dcc36d8cb6d2464a5c9b2d
jcooklin <joel.cooklin@gmail.com>
2013-11-01 15:40:45 -0700

    added full path for start service

--- tools/install_upstart.sh
+++ tools/install_upstart.sh
@@ -3,10 +3,10 @@
 cp ttm.upstart /etc/init/ttm.conf
 ln -s /lib/init/upstart-job /etc/init.d/ttm
 echo "starting ttm service"
-start ttm
+/sbin/start ttm
 
 cp ttm-ssl.upstart /etc/init/ttm-ssl.conf
 ln -s /lib/init/upstart-job /etc/init.d/ttm-ssl
 echo "starting ttm-ssl service"
-start ttm-ssl
+/sbin/start ttm-ssl
 

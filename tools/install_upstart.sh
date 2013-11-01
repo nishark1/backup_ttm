cc2d975abf7e192c17320f8a7e7e64eb4bdb192c
root <root@hznnode01.novalocal>
2013-11-01 10:15:26 -0700

    added ssl endpoint to upstart install

--- tools/install_upstart.sh
+++ tools/install_upstart.sh
@@ -4,10 +4,9 @@ cp ttm.upstart /etc/init/ttm.conf
 ln -s /lib/init/upstart-job /etc/init.d/ttm
 echo "starting ttm service"
 start ttm
-echo "that's all folks"
 
-#cp ism.upstart /etc/init/ism.conf
-#ln -s /lib/init/upstart-job /etc/init.d/ism
-#echo "starting ism service"
-#start ism
-#echo "that's all folks"
+cp ttm-ssl.upstart /etc/init/ttm-ssl.conf
+ln -s /lib/init/upstart-job /etc/init.d/ttm-ssl
+echo "starting ttm-ssl service"
+start ttm-ssl
+

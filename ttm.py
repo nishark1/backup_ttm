b7e211493d07c44ca7b26cb4420e96203038aa53
nkadakia <nisha_rk1@yahoo.com>
2014-01-21 07:00:05 +0000

    Added API for recipes start and end times and handle bootstrap start time

--- ttm.py
+++ ttm.py
@@ -197,6 +197,8 @@ def create_metric(instance_id, date_in_isoformat):
         #if metric exists we do not want to create again
         if (metric):
             ttm_logger.debug('Metric already exists')
+            #The second time we are called is from within the bootstrap :)
+            metric["bootstrap_start_time"] = date_in_isoformat
             pass
         else:
             ttm_logger.debug('creating Metric')
@@ -210,6 +212,7 @@ def create_metric(instance_id, date_in_isoformat):
                 "instance_id": instance_id,
                 "instance_name": "",
                 "start_time": date_in_isoformat,
+                "bootstrap_start_time": "",
                 "end_time": "",
                 "instance_start_time": date_in_isoformat,
                 "bootstrap_timeout":False
@@ -359,6 +362,55 @@ def update_metric(instance_id):
         raise
 
     return jsonify( { 'metric': metric } ), 201
+
+@app.route('/ttm/api/v1.0/instances/<string:instance_id>/recipe/<string:recipe_name>', methods=['POST'])
+def update_recipe(instance_id, recipe_name):
+    ttm_logger.info('Entering update_recipe' )
+    #This is called by utc_time
+    metric = {} 
+    try:
+        if ( instance_id != None and instance_id.find('\"') != -1):
+            instance_id = instance_id.replace('\"','')
+        
+        date = datetime.utcnow()
+        metric_id_array = r_server.lrange(instance_id, 0, -1)
+        if (metric_id_array):
+            metric_id = metric_id_array[0]
+            metric = r_server.hgetall(metric_id)
+
+        if (metric):
+            if (not metric.has_key("recipes")):
+                metric["recipes"] = {}
+
+            if(len(metric["recipes"]) > 0):
+                import ast
+                dict_recipes = ast.literal_eval(metric["recipes"])
+
+                if(dict_recipes.has_key(recipe_name)):
+                    dict_recipes[recipe_name]["recipe_end_time"] = date.isoformat()
+                    if (request.form.has_key("msg")):
+                        dict_recipes[recipe_name]["end_msg"] = request.form["msg"]
+                else:
+                    dict_recipes[recipe_name] = {}
+                    dict_recipes[recipe_name]["recipe_start_time"] = date.isoformat()
+                    if (request.form.has_key("msg")):
+                        dict_recipes[recipe_name]["start_msg"] = request.form["msg"]
+
+                metric["recipes"] = dict_recipes
+            else:
+                metric["recipes"][recipe_name] = {}
+                metric["recipes"][recipe_name]["recipe_start_time"] = date.isoformat()
+                if (request.form.has_key("msg")):
+                    metric["recipes"][recipe_name]["start_msg"] = request.form["msg"]
+
+            r_server.hmset(metric_id, metric)
+        else:
+            ttm_logger.debug("a recipe {0} was provided but we don't have a a metric for instance_id {1}".format(recipe_name, instance_id))
+    except Exception as e:
+        ttm_logger.exception("Exception occured in update_recipe")
+
+    return jsonify( { 'metric': metric } ), 201
+
 
 if __name__ == '__main__':
 
